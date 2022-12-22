d8088129c0be9ffba55ba490df4f866f587f398a
Tom Trafford <maks.kachorak@gmail.com>
2022-12-22 22:21:26 +0200

    hi

---
+++ src/main/java/net/ttrafford/bindme/BindME.java
@@ -0,0 +1,14 @@
+package net.ttrafford.bindme;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class BindME implements ModInitializer {
+	public static final String MOD_ID = "bindme";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
